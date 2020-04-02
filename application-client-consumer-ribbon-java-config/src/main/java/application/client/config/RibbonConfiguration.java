37ec6d10e75817734ca8120859ae1a571668af3f
Ouroborosi <wa104c1200332@gmail.com>
2020-04-02 08:32:12 +0800

    refactor: remove @Configuration and make comment more precisely

--- application-client-consumer-ribbon-java-config/src/main/java/application/client/config/RibbonConfiguration.java
+++ application-client-consumer-ribbon-java-config/src/main/java/application/client/config/RibbonConfiguration.java
@@ -3,13 +3,10 @@ package application.client.config;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Ribbon Configuration Class
- * !! Beware: This class has to be a @Configuration class and should not in a @ComponentScan for the main application context.
  */
-@Configuration
 public class RibbonConfiguration {
     @Bean
     public IRule ribbonRule() {
